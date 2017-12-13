0f6b438155c19e40833615f2b8419ae1ecb02d6d
Piotr Leoszewski <p.leoszewski@gmail.com>
2017-12-13 09:07:02 +0100

    Added JS to navbar function.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function showElement(element) {
-
+function clicked(clickedButton) {
+    document.querySelector('.active').classList.remove('active');
+    document.querySelector(clickedButton).classList.add('active');
 }
+
+// (function attachListener() {
+//     var button = document.getElementsByClassName('button');
+//     button.addEventListener('click', function (event) {
+//         deactivate();
+//         document.getElementById('offer').classList.add('active');
+//     })
+// })();
+//
+// function deactivate() {
+//     document.querySelector('.active').classList.remove('active');
+// }
