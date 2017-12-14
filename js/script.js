99892f50224d823423be37cc484bc5f7dede2586
Piotr Leoszewski <p.leoszewski@gmail.com>
2017-12-14 14:34:30 +0100

    Fixed menu button.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
+let menuVisibility = document.getElementById('menu');
+
 function clicked(clickedButton) {
     document.querySelector('.active').classList.remove('active');
     document.querySelector(clickedButton).classList.add('active');
+    menuVisibility.style.visibility = 'hidden';
 }
 
-// (function attachListener() {
-//     var button = document.getElementsByClassName('button');
-//     button.addEventListener('click', function (event) {
-//         deactivate();
-//         document.getElementById('offer').classList.add('active');
-//     })
-// })();
-//
-// function deactivate() {
-//     document.querySelector('.active').classList.remove('active');
-// }
+function clickedMenuButton() {
+    if (menuVisibility.style.visibility === '') {
+        menuVisibility.style.visibility = 'visible';
+    } else if (menuVisibility.style.visibility === 'hidden') {
+        menuVisibility.style.visibility = 'visible';
+    } else {
+        menuVisibility.style.visibility = 'hidden';
+    }
+}
